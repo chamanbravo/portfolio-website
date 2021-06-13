7e5d95ff1f253033bd998f675e1648886888ff7f
chaman <chamanpro9@gmail.com>
2021-06-13 23:36:30 +0545

    nav btn

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+//copyright text
 document.querySelector(".copyright-txt").innerHTML = `© ${new Date().getFullYear()} Chaman Bravo`;
+
+//nav btn
+var navbtn = document.querySelector(".navbtn");
+
+navbtn.addEventListener("click", openMenu);
+
+function openMenu() {
+  navbtn.classList.toggle("navclicked");
+  console.log("Kina vayena");
+}
