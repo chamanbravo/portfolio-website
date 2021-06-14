5f18934d2eb76257632e61c49a247eb2e5496819
chaman <chamanpro9@gmail.com>
2021-06-14 18:11:20 +0545

    menu

--- script.js
+++ script.js
@@ -3,10 +3,41 @@ document.querySelector(".copyright-txt").innerHTML = `© ${new Date().getFullYea
 
 //nav btn
 var navbtn = document.querySelector(".navbtn");
+var overlay = document.querySelector(".overlay");
+var body = document.querySelector("body");
+var menuItems = document.querySelector(".menu-items");
 
 navbtn.addEventListener("click", openMenu);
 
 function openMenu() {
   navbtn.classList.toggle("navclicked");
-  console.log("Kina vayena");
+  overlay.classList.toggle("overlayclose");
+  body.classList.toggle("overflow");
+  menuItems.classList.toggle("menuopen");
 }
+
+
+// custom cursor
+const cursor = document.querySelector('.cursor');
+window.onmousemove = (e) => {
+    cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`);
+}
+
+window.addEventListener('mousedown', mouseDownHandler);
+window.addEventListener('mouseup', mouseUpHandler);
+
+function mouseUpHandler(e) {
+  cursor.style.transform = "scale(1)";
+
+  if (e.target.classList.contains('link')) {
+    cursor.style.transform = 'scale(5)';
+    cursor.style.opacity = 0;
+  }
+}
+
+function mouseDownHandler() {
+  cursor.style.transform = "scale(1.5)";
+}
+
+
+//skew scroll
