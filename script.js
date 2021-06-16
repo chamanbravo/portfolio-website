4c9768bd00007575b9e79b6033035e6f9f87615c
chaman <chamanpro9@gmail.com>
2021-06-16 15:51:30 +0545

    nav

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
 //copyright text
 document.querySelector(".copyright-txt").innerHTML = `© ${new Date().getFullYear()} Chaman Bravo`;
 
-//nav btn
-var navbtn = document.querySelector(".navbtn");
-var body = document.querySelector("body");
-var menuItems = document.querySelector(".menu-items");
-
-navbtn.addEventListener("click", openMenu);
-
-//gsap
-const tl = gsap.timeline({paused:true, reversed: true});
-tl.to('.box', {height: '100vh', duration: .5, transformOrigin: 'bottom', stagger: .3})
-tl.to('.menu-items', {opacity: '1', visibility: 'visible', duration: .5, stagger: .3})
-
-function openMenu() {
-  navbtn.classList.toggle("navclicked");
-  body.classList.toggle("overflow");
-  if(tl.reversed()){
-    tl.play();
-  }else{
-    tl.reverse();
-  }
-}
-
-
 // custom cursor
 const cursor = document.querySelector('.cursor');
 window.onmousemove = (e) => {
-    cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`);
+    cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px;`);
 }
 
 window.addEventListener('mousedown', mouseDownHandler);
@@ -46,6 +23,37 @@ function mouseDownHandler() {
   cursor.style.transform = "scale(1.5)";
 }
 
+//nav btn
+var navbtn = document.querySelector(".navbtn");
+var body = document.querySelector("body");
+var menuItems = document.querySelector(".menu-items");
+
+navbtn.addEventListener("click", openMenu);
+
+//gsap
+const tl = gsap.timeline({paused:true, reversed: true});
+tl.to('.box', {height: '100vh', duration: .5, transformOrigin: 'bottom', stagger: .3})
+tl.to('.menu-items', {opacity: '1', visibility: 'visible', duration: .5, stagger: .3})
+
+function openMenu() {
+  navbtn.classList.toggle("navclicked");
+  body.classList.toggle("overflow");
+  cursor.classList.toggle("overlayCursor");
+  if(tl.reversed()){
+    tl.play();
+  }else{
+    tl.reverse();
+  }
+}
+
+var links = document.querySelector(".links");
+
+links.addEventListener('click', () => {
+    tl.reverse();
+    navbtn.classList.toggle("navclicked");
+    body.classList.toggle("overflow");
+    console.log("Whaaat");
+});
 
 //skew scroll
 const content = document.querySelector(".scroll-wrapper");
